17e0680171494a7bdf4948098bb58908fb4eb137
Adithya <adithyaudayo05@gmail.com>
2025-03-15 23:25:43 +0000

    Add project files.

---
+++ AspNetCoreWebApi/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using AspNetCoreWebApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ShopContext _context;
+        public ProductsController(ShopContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _context.Products.ToArray(); 
+        }
+    }
+}
